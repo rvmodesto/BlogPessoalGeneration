38a162195875d4abacbb567b798febe6b1e4435c
Raquel Modesto <raquel.modesto2@gmail.com>
2021-08-25 14:25:13 -0300

    Atualização #9 CRUD

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
@@ -1,11 +1,17 @@
 package br.org.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +20,52 @@ import br.org.generation.blogpessoal.repository.PostagemRepository;
 
 @RestController
 @RequestMapping("/postagens")
-@CrossOrigin(origins = "+", allowedHeaders = "*")
+@CrossOrigin(origins = "+", allowedHeaders = "*")//cabeçalho para backend
 public class PostagemController {
 
 	@Autowired
-	private PostagemRepository postagemRepository;
+	private PostagemRepository postagemRepository; //objeto interface repository
 	
 	@GetMapping
 	public ResponseEntity <List<Postagem>> GetAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+	
+	@GetMapping("idifelse/{id}")
+	public ResponseEntity<Postagem> getByIdIfElse(@PathVariable long id){
+			
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+	
+		if (postagem.isPresent()) {
+			return ResponseEntity.ok(postagem.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+// Postagem :: postagem == Postagem postagem = new Postagem()
+	@GetMapping("/{id}") //lambda
+	public ResponseEntity<Postagem> getById(@PathVariable long id){
+		return postagemRepository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/titulo{titulo}")
+		public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+			return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> postPostagem(@RequestBody Postagem postagem){
+			return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> putPostagem(@RequestBody Postagem postagem){
+			return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletePostagem(@PathVariable long id) {//sem retorno
+			postagemRepository.deleteById(id);
 	}
 }
