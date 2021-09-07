31f4b17261a9be80397138277d6379df812e2626
Raquel Modesto <raquel.modesto2@gmail.com>
2021-09-07 20:19:46 -0300

    Adicionando Security

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -39,8 +39,11 @@ public class Postagem {
 	@JsonIgnoreProperties("postagem")
 	private Tema tema; 
 
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario; 
 
-
+	
 	public long getId() {
 		return id;
 	}
@@ -79,6 +82,14 @@ public class Postagem {
 
 	public void setTema(Tema tema) {
 		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 	
 }
